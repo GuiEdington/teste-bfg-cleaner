32ed85554768229a63dd707eb028b21aacf65c14
GuiEdington <guilhermeedington@gmail.com>
2025-04-16 15:03:07 -0300

    feat: new keys

---
+++ new_file.py
@@ -0,0 +1,2 @@
+password_2 = '##456teste_konsi789'
+print(password_2)
--- teste.py
+++ teste.py
@@ -1,4 +1,6 @@
 # testando salvamento de secrets: senha=##123teste_konsi123
 
 password = "##123teste_konsi123"
+password_2 = '##456teste_konsi789'
+# nova senha ##456teste_konsi789
 pass
